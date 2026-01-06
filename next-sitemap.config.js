94f48fca3ecf86993b029e0bab97a8e373d1fd8f
Vineet <vernorexiasharma@gmail.com>
2026-01-06 14:00:13 +0530

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.itss.co.in',
+    generateRobotsTxt: true,
+    // Optional: Add other configurations like exclude, robotsTxtOptions, etc.
+}
