cedac472344f611bb34119bb8c36c08bf6c199b5
luckykashyap4219 <124385681+lucshi676@users.noreply.github.com>
2026-01-21 15:59:44 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   description: "Leading commercial interior designers delivering turnkey office & corporate interior solutions across India.",
   keywords: "commercial interior design company, commercial interior designers, commercial interior solutions, turnkey commercial interior company, corporate office interiors, best commercial interior design firm, commercial interior design company in india, office interior design company, turnkey interior contractors, commercial interior designers india, turnkey office interior solutions, interior design company india",
  alternates: {
-    canonical: "https://itss.co.in/"
+    canonical: "https://www.itss.co.in/"
   } 
 };
 
