712199e10ace78dba2a903058deb501912ebd004
luckykashyap4219 <124385681+lucshi676@users.noreply.github.com>
2026-01-22 17:14:46 +0530

    Update page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
     title: "Commercial Interior Design Services Across India | ITSS ",
     description: "ITSS is a leading commercial interior design company in India providing office interior design, turnkey commercial interiors, and corporate fit-out services across India.",
     keywords: "office interior design company, office interior designers, office fit out services, turnkey office interior solutions, corporate office interior design, workspace interior design services, modern office interior design services, office renovation contractor, commercial renovation contractor, turnkey interior contractor for office, commercial interior design services, office interior design company, turnkey commercial interiors, corporate fit out services, office interior designers india",
-};
+],
  alternates: {
     canonical: "https://www.itss.co.in/services",
 };
