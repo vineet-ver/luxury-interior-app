30a36f93a3b4d118e9d830e2fd93c39cb00a683a
luckykashyap4219 <124385681+lucshi676@users.noreply.github.com>
2026-01-22 16:53:57 +0530

    Update page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,9 +3,19 @@ import type { Metadata } from "next";
 import ServicesClient from "@/components/services/ServicesClient";
 
 export const metadata: Metadata = {
-    title: "Office Interior Design & Turnkey Fit-Out Services",
-    description: "Office interior design company offering corporate interiors, renovation & turnkey office fit-out services.",
-    keywords: "office interior design company, office interior designers, office fit out services, turnkey office interior solutions, corporate office interior design, workspace interior design services, modern office interior design services, office renovation contractor, commercial renovation contractor, turnkey interior contractor for office",
+    title: "Commercial Interior Design Services Across India | ITSS ",
+    description: "ITSS is a leading commercial interior design company in India providing office interior design, turnkey commercial interiors, and corporate fit-out services across India.",
+    keywords: "office interior design company, office interior designers, office fit out services, turnkey office interior solutions, corporate office interior design, workspace interior design services, modern office interior design services, office renovation contractor, commercial renovation contractor, turnkey interior contractor for office, commercial interior design services, office interior design company, turnkey commercial interiors, corporate fit out services, office interior designers india",
+};
+ alternates: {
+    canonical: "https://www.itss.co.in/services",
+};
+robots: {
+    index: true,
+    follow: true,
+  },
+authors: [{ name: "ITSS Interior Experts" }],
+  publisher: "India Technical Solution Services",
 };
 
 export default function ServicesPage() {
