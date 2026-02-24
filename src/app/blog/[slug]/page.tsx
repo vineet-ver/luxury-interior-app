10f6c14c90ad593bc93edb6b92455b5bb95bdc06
luckykashyap4219 <124385681+lucshi676@users.noreply.github.com>
2026-02-24 11:16:04 +0530

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,35 @@
+import { notFound } from "next/navigation";
+import { blogPosts } from "@/data/blogs";
+
 export default function BlogPost({
   params,
 }: {
   params: { slug: string };
 }) {
+
+  const post = blogPosts.find(
+    (item) => item.slug === params.slug
+  );
+
+  if (!post) {
+    notFound();
+  }
+
   return (
-    <div style={{ padding: "100px" }}>
-      <h1>Blog Slug: {params.slug}</h1>
+    <div className="max-w-4xl mx-auto px-6 py-20">
+      <h1 className="text-4xl font-bold mb-6">
+        {post.title}
+      </h1>
+
+      <img
+        src={post.image}
+        alt={post.title}
+        className="w-full rounded-xl mb-8"
+      />
+
+      <div className="whitespace-pre-line">
+        {post.content}
+      </div>
     </div>
   );
 }
