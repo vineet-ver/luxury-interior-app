f82ea0fabb3e0623afa600bd79c89859ef865fb6
luckykashyap4219 <124385681+lucshi676@users.noreply.github.com>
2026-02-24 11:24:10 +0530

    Create blogs.ts

---
+++ src/data/blogs.ts
@@ -0,0 +1,14 @@
+export const blogPosts = [
+  {
+    slug: "test-blog",
+    title: "Test Blog Post",
+    description: "This is a test blog description.",
+    image: "/blog/corporate.jpg",
+    category: "Test",
+    content: `
+This is a test blog content.
+
+Everything is working properly.
+`
+  }
+];
