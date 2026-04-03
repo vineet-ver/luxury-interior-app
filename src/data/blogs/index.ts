c9480857ea37346899942ab93e1861d61e65a263
luckykashyap4219 <124385681+lucshi676@users.noreply.github.com>
2026-04-03 15:28:41 +0530

    Update index.ts

--- src/data/blogs/index.ts
+++ src/data/blogs/index.ts
@@ -1,9 +1,27 @@
-import { interiorCostBlog } from "./interior-cost";
-import { officeDesignBlog } from "./office-design";
-import { careerBlog } from "./career";
+import officeIdeas from "./office-ideas";
+import biophilic from "./biophilic-design";
+import luxuryDesign from "./luxury-design";
+import interiorCost from "./interior-cost";
+import principles from "./design-principles";
+import courses from "./design-courses";
+import compare from "./designer-vs-decorator";
+import chooseDesigner from "./choose-designer";
+import homeIdeas from "./home-ideas";
+import officeCabin from "./office-cabin";
+import smallOffice from "./small-office";
+import trends from "./design-trends";
 
 export const blogs = [
-  interiorCostBlog,
-  officeDesignBlog,
-  careerBlog,
+  officeIdeas,
+  biophilic,
+  luxuryDesign,
+  interiorCost,
+  principles,
+  courses,
+  compare,
+  chooseDesigner,
+  homeIdeas,
+  officeCabin,
+  smallOffice,
+  trends,
 ];
